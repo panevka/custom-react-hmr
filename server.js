5b1f15e4883c5fdb32776efdea517b2c51bcbf0e
panevka <97028404+panevka@users.noreply.github.com>
2025-08-07 09:29:21 +0200

    feat: add watching for file changes with chokidar

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import chokidar from "chokidar";
 
 const PORT = 3000;
 const app = express();
@@ -8,3 +9,12 @@ app.use(express.static(import.meta.dirname));
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+const watcher = chokidar.watch("./", {
+  ignored: (path, stats) =>
+    path.startsWith("node_modules") ||
+    (stats?.isFile() && !path.endsWith(".js")),
+  persistent: true,
+});
+
+watcher.on("change", (path) => console.log(`File ${path} has been changed`));
