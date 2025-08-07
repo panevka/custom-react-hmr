6e92c734fb0469819c17082cd616c4a032eee22e
panevka <97028404+panevka@users.noreply.github.com>
2025-08-07 10:11:24 +0200

    feat: add draft hmr middleware

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import chokidar from "chokidar";
 import http from "node:http";
-import { WebSocket, WebSocketServer } from "node:http";
+import fs from "node:fs";
+import path from "node:path";
+import { WebSocket, WebSocketServer } from "ws";
 
 const PORT = 3000;
 const app = express();
@@ -22,8 +24,31 @@ const watcher = chokidar.watch("./", {
 
 watcher.on("change", (path) => console.log(`File ${path} has been changed`));
 
+const hmrMiddleware = async (req, res, next) => {
+  if (!req.url.endsWith(".js")) return next();
+
+  const requestedFile = await fs.readFile(
+    path.join(process.cwd(), req.url),
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(data);
+    },
+  );
+
+  console.log(req.url);
+  console.log(requestedFile);
+  next();
+
+  res.send(requestedFile);
+};
+
+app.use(hmrMiddleware);
 app.use(express.static(import.meta.dirname));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
