c3deb41c59dd9a2bfec97e786ea1d8f4f6cc661f
panevka <97028404+panevka@users.noreply.github.com>
2025-08-07 09:45:03 +0200

    feat: create a websocket server

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import express from "express";
 import chokidar from "chokidar";
+import http from "node:http";
+import { WebSocket, WebSocketServer } from "node:http";
 
 const PORT = 3000;
 const app = express();
+const server = http.createServer(app);
+const ws = new WebSocketServer({ server });
+
+let socket;
+ws.on("connection", (_socket) => {
+  socket = _socket;
+});
 
 app.use(express.static(import.meta.dirname));
 
