e5407721558da3a8e8a46649660b7b0a68d6ac38
panevka <97028404+panevka@users.noreply.github.com>
2025-08-13 10:12:02 +0200

    feat: append child to main document for preview purposes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
+import { Child } from "./child.js";
+
 export function mount() {
   const $app = document.querySelector("#app");
   const now = new Date().toLocaleTimeString();
   $app.innerText = `Current date: ${now}`;
+  $app.appendChild(Child());
 }
